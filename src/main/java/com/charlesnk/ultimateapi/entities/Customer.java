c5d4ded869ec00e6d190d9bdc9d60baea9561e79
noucharles <noucharles@gmail.com>
2020-11-22 19:29:29 +0100

    Configuration de la base de données

--- src/main/java/com/charlesnk/ultimateapi/entities/Customer.java
+++ src/main/java/com/charlesnk/ultimateapi/entities/Customer.java
@@ -1,13 +1,72 @@
 package com.charlesnk.ultimateapi.entities;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 
 @Entity
 public class Customer {
 
+    @Id
+    @GeneratedValue
     private int id;
 
     private String firstName;
 
     private String lastName;
+
+    private String email;
+
+    private String company;
+
+    public Customer() {
+    }
+
+    public Customer(int id, String firstName, String lastName, String email, String company) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.company = company;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getCompany() {
+        return company;
+    }
+
+    public void setCompany(String company) {
+        this.company = company;
+    }
 }
